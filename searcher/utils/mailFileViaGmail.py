6879ccda74022db8110025afe9188e1a89a563c1
johnb <jbragato@yahoo.com>
2014-07-12 23:06:56 -0400

    x

--- searcher/utils/mailFileViaGmail.py
+++ searcher/utils/mailFileViaGmail.py
@@ -10,91 +10,69 @@ import os
 
 
 def send_gmail(to, subject, text, attach):
-   msg = MIMEMultipart()
+    msg = MIMEMultipart()
 
-   msg['From'] = gmail_user
-   msg['To'] = to
-   msg['Subject'] = subject
+    msg['From'] = gmail_user
+    msg['To'] = to
+    msg['Subject'] = subject
 
-   msg.attach(MIMEText(text))
+    msg.attach(MIMEText(text))
 
-   part = MIMEBase('application', 'octet-stream')
-   part.set_payload(open(attach, 'rb').read())
-   Encoders.encode_base64(part)
-   part.add_header('Content-Disposition',
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(open(attach, 'rb').read())
+    Encoders.encode_base64(part)
+    part.add_header('Content-Disposition',
            'attachment; filename="%s"' % os.path.basename(attach))
-   msg.attach(part)
+    msg.attach(part)
 
-   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
-   mailServer.ehlo()
-   mailServer.starttls()
-   mailServer.ehlo()
-   mailServer.login(gmail_user, gmail_pwd)
-   mailServer.sendmail(gmail_user, to, msg.as_string())
-   # Should be mailServer.quit(), but that crashes...
-   mailServer.close()
+    mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(gmail_user, gmail_pwd)
+    mailServer.sendmail(gmail_user, to, msg.as_string())
+    # Should be mailServer.quit(), but that crashes...
+    mailServer.close()
 
 ################# RUN ##################
-def main():
+def main(toaddrs=None, msgsubj=None, bodyfile=None, attachfile=None, gmail_pwd=None):
 
-  import re,os,sys
+    import re,os,sys
 
+    gmail_user  =   'john.bragato@gmail.com'
+    gmail_pwd   =   ''
 
-  gmail_user = "john.bragato@gmail.com"
-  gmail_pwd = "yankee17"
+    if not toaddrs:
+        toaddrs =   'john.bragato@bluefly.com'
 
-  #for arg in sys.argv[1]:
-  #    print arg
-  #    toaddrs_list = []
-  #    pattern_email = re.compile(r'.+[@].+[.].+')
-  #    to_emailargs = re.findall(pattern_email, arg)
-  #    if to_emailargs:
-  #        for line in to_emailargs:
-  #            toaddrs_list.append(line)
-  #    else:
-  #        print "Fail"
-  #
+    if bodyfile:
+        msgbody         =   open(bodyfile, 'rb').read()
+        msgsubj = "File: {0} was attached ByPython".format(os.path.basename(bodyfile))
+    else:
+        msgbody = "File: was attached By Python"
+        if not msgsubj:
+            msgsubj =   msgbody[:15]
 
 
-  ## Test for HTML File is at sys.argv[3] to insert as Body of email
-  try:
-      if sys.argv[2]:
-          test = sys.argv[2]
-          if os.path.isfile(test):
-              bodyfile    = sys.argv[2]
-              msgsubj     = "The Daily Completion Manifesto"
-  ### Try to make body of message render HTML
-              try:
-                  msgbody     = open(bodyfile, 'rb').read()
-              except NameError:
-                  msgbody      = "NameErrorTestBody"
-  #                msgsubj      = "File: {0} was attached ByPython".format(os.path.basename(sys.argv[1]))
-  #                toaddrs      = "john.bragato@bluefly.com"
-              if sys.argv[3]:
-                  toaddrs     = "john.bragato@bluefly.com"
-              else:
-  			    toaddrs     = "john.bragato@bluefly.com"
-          else:
-  			toaddrs     = sys.argv[2]
-  			msgbody     = "TestBody"
-  			msgsubj     = "SentByPython"
-  except IndexError:
-      toaddrs     = "john.bragato@bluefly.com"
-      msgsubj    = "File: {0} was attached ByPython".format(os.path.basename(sys.argv[1]))
-      msgbody     = "TestBody"
+    if os.path.isfile(attachfile):
+        send_gmail(toaddrs, msgsubj, msgbody, attachfile)
 
-  ## Test for HTML File to insert as Body of email
+    elif os.path.isfile(bodyfile):
+        send_gmail(toaddrs, msgsubj, msgbody, bodyfile)
 
-  ## Include file as first arg as an attachment to mail
-  try:
-      attachfile   = sys.argv[1]
-      if os.path.isfile(attachfile):
-          send_gmail(toaddrs, msgsubj, msgbody, attachfile)
-      else:
-          send_gmail(toaddrs, msgsubj, msgbody, "NotAfile")
-  except:
-      print "Failed to Send File. Make Sure a valid file is your 1st Arg"
-
+    else:
+        send_gmail(toaddrs, msgsubj, msgbody, "NotAfile")
+    #for arg in sys.argv[1]:
+    #    print arg
+    #    toaddrs_list = []
+    #    pattern_email = re.compile(r'.+[@].+[.].+')
+    #    to_emailargs = re.findall(pattern_email, arg)
+    #    if to_emailargs:
+    #        for line in to_emailargs:
+    #            toaddrs_list.append(line)
+    #    else:
+    #        print "Fail"
+    #
 
 if __name__ == "__main__":
     main()
