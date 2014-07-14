2c2823a445ee7f2acf0a5922955bd385d558897c
relic7 <relic7@users.noreply.github.com>
2014-07-13 20:58:00 -0400

    Update home.js

--- base/static/js/home.js
+++ base/static/js/home.js
@@ -16,6 +16,26 @@ $(document).ajaxStart(function() {
     $('#spinner').hide();
 });
 
+/////
+$("#input_list_form").submit(function() {
+
+    //var url = 'http://prodimages.ny.bluefly.com/api/v1/excel-tool-data/'; // the script where you handle the form input.
+    // var url = 'http://prodimages.ny.bluefly.com/searcher/ajax/colorstyle/';
+    //var url = '{% url 'searcher:views-ajax_colorstyle_search' %}';
+    var url = 'searcher/runscripts/script_runner_home';
+    $.ajax({
+           type: "get",
+           url: url,
+           data: $("#input_list_form").serialize(), // serializes the form's elements.
+           success: function(data) {
+                    alert(data); // show response from the python script.
+                    }
+         });
+
+    return false; // avoid to execute the actual submit of the form.
+});
+ 
+
 //// Date picker popup calendar 
 $('#daterange-container').find('.input-daterange').datepicker({
           todayBtn: true,
