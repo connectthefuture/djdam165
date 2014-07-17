a3524a477dfbf230af9add17346ea0d5d5c33201
johnb <jbragato@yahoo.com>
2014-07-17 16:18:36 -0400

    x

--- base/views.py
+++ base/views.py
@@ -37,6 +37,6 @@ def testjsonform(request):
         # validate and save
         pass
 
-    template = 'test_json_form.html'
+    template = 'djdam/base/test_json_form.html'
     context = RequestContext(request, {'form': form})
     return render_to_response(template, context)
