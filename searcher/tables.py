cab7c23089f1b5a4cdab743fc712ccec0766d448
johnb <jbragato@yahoo.com>
2014-07-28 16:39:00 -0400

    c

--- searcher/tables.py
+++ searcher/tables.py
@@ -3,20 +3,35 @@ __author__ = 'johnb'
 import django_tables2 as tables
 from searcher.models import SupplierIngest
 
-# Def Tables -- aka tables.py
+####  Tables -- aka tables.py
 class SupplierIngestTable(tables.Table):
+    vendor_name  =  tables.Column()
+    vendor_brand =  tables.Column()
+    image_url	 =	tables.URLColumn()
+    alt			 =	tables.Column()
+    image_type 	 = 	tables.Column()
+
+
     class Meta:
         model = SupplierIngest
         # add class="paleblue" to <table> tag
         attrs = {"class": "paleblue"}
 
 
-##### VIEWS ####
+
+##### Table VIEWS ####
 from django.shortcuts import render
 from django_tables2   import RequestConfig
 
 
+def get_http_status_code(request):
+    import requests
+
+    r = requests.get(request.G)
+    code = r.status_code
+    return code
+
 def suppliers(request):
     table = SupplierIngestTable(SupplierIngest.objects.all())
     RequestConfig(request).configure(table)
-    return render(request, 'supplier-ingest-styles.html', {'table': table})
+    return render(request, 'tables/supplier-ingest-styles.html', {'table': table})
