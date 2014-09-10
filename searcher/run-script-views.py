6587eebb6ebd10de924f2a1abe6bb19dbf948abf
johnb <john.bragato@gmail.com>
2014-09-10 17:23:02 -0400

    revert

--- searcher/run-script-views.py
+++ searcher/run-script-views.py
@@ -44,8 +44,7 @@ def script_runner_home_page(request):
     except:
         
         try:
-            styles = request.GET['input_list'].split(' ')
-            #styles= request.GET('q')
+            styles= request.GET('q')
         except IndexError:        
             message = 'You submitted an empty list of styles. Please try again.'
             return HttpResponseRedirect(message=message, redirect_to='#')
@@ -68,19 +67,11 @@ def script_runner_home_page(request):
                 res = download_server_imgs_byPOorStyleList.main(styles_list=styles)
 
         elif script_selected == 'newAll_Sites_CacheClear.py':
-            if len(styles) < 10 and type(styles) != list:
-                regex_colorstyle=re.compile(r'(\d{9})')
-                styles = list(set([ n for n in regex_colorstyle.split(str(styles)) if n.strip(' ') ]))
-
-                #styles = list(set([ n for n in regex_colorstyle.split(styles) if n.strip(' ') ]))
-                
-                #styles = list(set(str(styles).split()))
+            if len(styles) < 10:
+                styles = list(set(str(styles).split()))
                 # re.split(r'\d{9}', styles)
             else:
-                #styles = list(set(str(styles).split()))
-                regex_colorstyle=re.compile(r'(\d{9})')
-                styles = list(set([ n for n in regex_colorstyle.split(str(styles)) if n.strip(' ') ]))
-                #styles = list(set([ n for n in regex_colorstyle.split(styles) if n.strip(' ') ]))
+                styles = list(set(str(styles).split()))
                 #styles = styles # ''.join(styles)
             script_selected = os.path.join('searcher/utils/', script_selected)
             # res = subprocess.call([script_selected, styles_list=styles])
@@ -112,14 +103,13 @@ def script_runner_home_page(request):
 
     if script_selected and styles:
         print 'ENTER SUBPROC'
-        print styles
         import subprocess
         ## Run the script here
         # abs_exec_scriptpath = os.path.join('/usr/local/batchRunScripts/python', 'script_selected')
         # results = subprocess.check_output([abs_exec_scriptpath, styles[:]]) # will then include results in return dict
 
         #return render_to_response('listing/script_output_page.html', {'styles': styles, 'script': script_selected, 'results': res}, context_instance=RequestContext(request))
-        return render(request, 'listing/script_output_page.html', {'styles': styles, 'script': script_selected, 'results': res.split() })
+        return render(request, 'listing/script_output_page.html', {'styles': styles, 'script': script_selected, 'results': res })
 
     else:
         message = 'Sorry, You Must have Done Something Wrong. Please check your input Data and try again.'
