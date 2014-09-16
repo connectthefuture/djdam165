c8cbf97eaf9c9d3b9a5cd43b90948939b914c6bb
johnb <john.bragato@gmail.com>
2014-09-16 12:23:48 -0400

    cx

--- imgadjust/urls.py
+++ imgadjust/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('imgadjust.views',
-    url(r'^$', 'index', name='index'),
+    url(r'^/?.*?(?P<inputColorstyle>.+)$', 'index', name='index'),
     url(r'^swap-images/$', 'swap_images', name='swap-images'),
     url(r'^add-replace-images/$', 'add_replace_images', name='add-replace-images'),
     url(r'^delete-images/$', 'delete_images', name='delete-images'),
