f531b3fb3cf532956ee3f02f23c291f87b5f4841
johnb <john.bragato@gmail.com>
2014-09-16 00:42:14 -0400

    models

--- imgadjust/models.py
+++ imgadjust/models.py
@@ -23,21 +23,25 @@ class Product(models.Model):
     colorstyle   = models.CharField(max_length=9)
     vendor_style = models.CharField(max_length=60)
     vendor_brand = models.CharField(max_length=60)
-    vendor_name =  models.CharField(max_length=60)
-    #imgsource = models.CharField(max_length=70, default="internal")
-    images = models.ManyToManyField(Image)
+    vendor_name  = models.CharField(max_length=60)
+    image_source = models.URLField(max_length=170, default="internal")
+    #image_source = models.CharField(max_length=70, default="internal")
+    #images = models.ManyToManyField(Image)
+    images      = models.ManyToManyField(Image)
     
     class Meta:
         db_table = 'product'
 
     def __unicode__(self):
-        return self.colorstyle
-
+        if self.image_source == 'internal': 
+            return self.colorstyle
+        else:
+            return self.image_source
 
 class ImageType(models.Model):
     colorstyle  = models.CharField(max_length=9)
-    alt     = models.IntegerField()
-    images  = models.ManyToManyField(Image)
+    alt         = models.IntegerField()
+    images      = models.ManyToManyField(Image)
     
     class Meta:
         db_table = 'style'
@@ -45,24 +49,34 @@ class ImageType(models.Model):
     def __unicode__(self):
         return self.alt
 
-
 ### file storage
 from django.core.files.storage import FileSystemStorage
 from djdam.settings import MEDIA_ROOT
 
 class Image(models.Model):
     from djdam.settings import MEDIA_ROOT
+    IMAGE_SIZES = (
+        (u'L', u'List'),
+        (u'P', u'PDP'),
+        (u'Z', u'Zoom'),
+    )
+    IMAGE_FORMATS = (
+        (u'jpg',  u'JPG'),
+        (u'png',  u'PNG'),
+        (u'tiff', u'TIFF'),
+        (u'psd',  u'PSD'),
+    )
+    
+    name = models.CharField(max_length=60)
+    image_size = models.CharField(max_length=2, choices=IMAGE_SIZES)
+    image_format = models.CharField(max_length=4, choices=IMAGE_FORMATS)
     colorstyle = models.ForeignKey(Product, to_field='colorstyle', related_name='images_colorstyle')
     alt        = models.ForeignKey(ImageType, to_field='alt', related_name='images_alt')
+    source_url = models.
     
+
     fs = FileSystemStorage(base_url='/images/', location='/media/uploads')
-
-    uploaded_img = models.ImageField(upload_to=upload_filepath,
-                              blank=True,
-                              null=True,
-                              height_field="height",
-                              width_field="width")
-
+    uploaded_img = models.ImageField(upload_to=upload_filepath, blank=True, null=True, height_field="height", width_field="width")
 
     class Meta:
         db_table = 'image'
@@ -74,10 +88,27 @@ class Image(models.Model):
         lst = [x.uploaded_img.name for x in self.uploaded_img.all()]
         lst = ["<a href='/media/uploads/{1}'>{0}</a>".format(x, x.split('/')[-1]) for x in lst]
         return ', '.join(lst)
-
     images.allow_tags = True
+
+
+    def get_absolute_url(self):
+        #from django.core.urlresolvers import reverse
+        #return reverse('people.views.details', args=[str(self.id)])
+        absurl = 'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=251&height=300'.format(self.colorstyle)
+        if int(self.alt) > 1:
+            absalt = 'http://cdn.is.bluefly.com/mgen/Bluefly/altimage.ms?img={0}_alt0{1}.jpg&w=120&h=144'.format(self.colorstyle, str(int(self.alt) - int()))
+            absurl = absalt
+        else:
+            pass
+        return absurl
+
+
+    def alt0(self):
+        return "_alt0{0}".format(str(int(self.alt) - int()))
+
     def _get_absolute_url(self):
-        return "/{0}/{1}/".format(MEDIA_ROOT, self.id)
+        return "/{0}/{1}/{2}/{3}".format(MEDIA_ROOT, self.colorstyle, self.alt, self.image_format)
+
 #       #(Whilst this code is correct and simple, it may not be the most portable way to write this kind of method. The reverse() function is usually the best approach.)
 #
 #     def get_absolute_url(self):
