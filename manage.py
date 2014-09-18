db168c8b959e82ea1da66202220713ddc74d30d6
johnb <john.bragato@gmail.com>
2014-09-18 17:15:14 -0400

    x

--- manage.py
+++ manage.py
@@ -3,9 +3,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-    ## Add Html Tidy Debug panel to syspat
-    sys.path.append(os.path.join((os.path.dirname(os.path.dirname(__file__)),'local/debug_toolbar_htmltidy' )))
-    #sys.path.append(os.path.abspath('/home/johnb/virtualenvs/DJDAM/local/debug_toolbar_html'))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djdam.settings")
 
     from django.core.management import execute_from_command_line
