6fd0b07f85c138e85fb7b3ebe2663e225f633f08
johnb <john.bragato@gmail.com>
2014-09-19 13:13:44 -0400

    x

--- djdam/wsgi.py
+++ djdam/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 ## Import New Relic Monitoring Services
 import newrelic.agent
-newrelic.agent.initialize('conf.newrelic.ini')
+newrelic.agent.initialize("conf/newrelic.ini")
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
