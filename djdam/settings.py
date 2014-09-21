bbee9e3b14f7615f44245973e2453c46e11fb8bd
johnb <john.bragato@gmail.com>
2014-09-21 15:33:33 -0400

    x

--- djdam/settings.py
+++ djdam/settings.py
@@ -191,11 +191,12 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.admindocs.middleware.XFrameOptionsMiddleware',
     'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
     'searcher.utils.Tastypie_Default_Format.TastyJSONMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
+
 
 
 TEMPLATE_DIRS = (
