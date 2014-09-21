43894ca434b22a1638c4a845dc38de837a1153d7
johnb <john.bragato@gmail.com>
2014-09-21 18:36:26 -0400

    x

--- imgadjust/models.py
+++ imgadjust/models.py
@@ -6,7 +6,7 @@
 ################################################################
 
 from django.db import models
-# from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 import json
 import floppyforms
 #
