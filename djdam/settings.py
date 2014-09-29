368934344a00374af73ebea6c7f009f584031602
johnb <john.bragato@gmail.com>
2014-09-29 17:33:32 -0400

    x

--- djdam/settings.py
+++ djdam/settings.py
@@ -209,7 +209,7 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates/ajaxsearch'),
     os.path.join(BASE_DIR, 'templates/accounts'),
     os.path.join(BASE_DIR, 'templates/imgadjust'),
-    os.path.join(BASE_DIR, 'templates/rest_framework'),
+    # os.path.join(BASE_DIR, 'templates/rest_framework'),
 )
 
 
