190ce16822acb77b10020fb9048d85b9aba82941
johnb <john.bragato@gmail.com>
2014-10-10 17:07:31 -0400

    x

--- searcher/admin.py
+++ searcher/admin.py
@@ -6,18 +6,32 @@ from models import *
 
 class ImageUpdateAdmin(myadmin.ModelAdmin):
     # This will generate a ModelForm
+    list_display = ('colorstyle', 'alt', 'modify_dt')
+    search_fields = ['colorstyle__alt']
     form = autocomplete_light.modelform_factory(ImageUpdate)
 myadmin.site.register(ImageUpdate, ImageUpdateAdmin)
 
 
 class SupplierIngestImagesAdmin(myadmin.ModelAdmin):
     # This will generate a ModelForm
+    list_display = ('colorstyle', 'alt', 'modify_dt')
+    search_fields = ['colorstyle__vendor_name']
     form = autocomplete_light.modelform_factory(SupplierIngestImages)
 myadmin.site.register(SupplierIngestImages, SupplierIngestImagesAdmin)
 
 
+class SupplierIngestAdmin(myadmin.ModelAdmin):
+    # This will generate a ModelForm
+    list_display = ('colorstyle', 'image_number', 'vendor_name', 'vendor_brand')
+    search_fields = ['colorstyle__vendor_name__vendor_brand']
+    form = autocomplete_light.modelform_factory(SupplierIngest)
+myadmin.site.register(SupplierIngest, SupplierIngestAdmin)
+
+
 class ProductSnapshotLiveAdmin(myadmin.ModelAdmin):
     # This will generate a ModelForm
+    list_display = ('colorstyle', 'brand', 'product_type', 'po_number', 'vendor_style', 'image_ready_dt')
+    search_fields = ['colorstyle__vendor_style__brand']
     form = autocomplete_light.modelform_factory(ProductSnapshotLive)
 myadmin.site.register(ProductSnapshotLive, ProductSnapshotLiveAdmin)
 
@@ -39,8 +53,16 @@ class SelectedFilesAdmin(myadmin.ModelAdmin):
     form = autocomplete_light.modelform_factory(SelectedFiles)
 myadmin.site.register(SelectedFiles, SelectedFilesAdmin)
 
+class OffshoreStatusAdmin(myadmin.ModelAdmin):
+    # This will generate a ModelForm
+    list_display = ('colorstyle', 'category', 'product_type', 'return_dt')
+    search_fields = ['colorstyle', 'send_dt', 'return_dt']
+    form = autocomplete_light.modelform_factory(OffshoreStatus)
+myadmin.site.register(OffshoreStatus, OffshoreStatusAdmin)
 
 class PostReadyOriginalAdmin(myadmin.ModelAdmin):
     # This will generate a ModelForm
+    list_display = ('colorstyle', 'alt', 'photo_date', 'file_path')
+    search_fields = ['colorstyle']
     form = autocomplete_light.modelform_factory(PostReadyOriginal)
 myadmin.site.register(PostReadyOriginal, PostReadyOriginalAdmin)
