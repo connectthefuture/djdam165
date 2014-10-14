90b3907ec8bb5e7b3d8bd1837c38f8881b8d2455
johnb <john.bragato@gmail.com>
2014-10-13 22:32:03 -0400

    x

--- searcher/admin.py
+++ searcher/admin.py
@@ -28,7 +28,9 @@ class SupplierIngestAdmin(myadmin.ModelAdmin):
     list_display = ('bfly_image', 'vendor_image', 'colorstyle', 'vendor_name', 'vendor_style', 'image_url', 'image_ready_dt', 'modified_dt', 'version', 'get_http_status_code')
     search_fields = ['colorstyle','vendor_name']
     list_filter = ('image_ready_dt', 'modified_dt', 'vendor_name')
-    form = autocomplete_light.modelform_factory(SupplierIngest)
+    #read_only_fields = ('bfly_image', 'vendor_image', 'colorstyle', 'vendor_name', 'vendor_style', 'image_url', 'image_ready_dt', 'modified_dt', 'version', 'get_http_status_code')
+
+form = autocomplete_light.modelform_factory(SupplierIngest)
     # def get_search_results(self, request, queryset, search_term):
     #     queryset, use_distinct = super(SupplierIngestAdmin, self).get_search_results(request, queryset, search_term)
     #     try:
