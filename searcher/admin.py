db6261bdb819585a00ebfae02d17734e336bb5ed
johnb <jbragato@yahoo.com>
2014-10-17 12:13:27 -0400

    comment out null filterer

--- searcher/admin.py
+++ searcher/admin.py
@@ -5,33 +5,33 @@ import autocomplete_light
 from models import *
 
 from django.contrib.admin.filters import SimpleListFilter
-
-class NullFilterSpec(SimpleListFilter):
-    title = u'ImageImported'
-
-    parameter_name = u'imported'
-
-    def lookups(self, request, model_admin):
-        return (
-            ('1', _('Has value'), ),
-            ('0', _('None'), ),
-        )
-
-    def queryset(self, request, queryset):
-        kwargs = {
-        '%s'%self.parameter_name : None,
-        }
-        if self.value() == '0':
-            return queryset.filter(**kwargs)
-        if self.value() == '1':
-            return queryset.exclude(**kwargs)
-        return queryset
-
-
-class StartNullFilterSpec(NullFilterSpec):
-    title = u'Started'
-    parameter_name = u'started'
-
+#
+# class NullFilterSpec(SimpleListFilter):
+#     title = u'ImageImported'
+#
+#     parameter_name = u'imported'
+#
+#     def lookups(self, request, model_admin):
+#         return (
+#             ('1', _('Has value'), ),
+#             ('0', _('None'), ),
+#         )
+#
+#     def queryset(self, request, queryset):
+#         kwargs = {
+#         '%s'%self.parameter_name : None,
+#         }
+#         if self.value() == '0':
+#             return queryset.filter(**kwargs)
+#         if self.value() == '1':
+#             return queryset.exclude(**kwargs)
+#         return queryset
+#
+#
+# class StartNullFilterSpec(NullFilterSpec):
+#     title = u'Started'
+#     parameter_name = u'started'
+#
 
 class ProductSnapshotLiveAdmin(myadmin.ModelAdmin):
     # This will generate a ModelForm
