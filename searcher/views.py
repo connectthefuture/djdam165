0e6f3ce32102c1e540bbece10fe350421f4385fd
johnb <john.bragato@gmail.com>
2014-11-03 13:05:16 -0500

    x

--- searcher/views.py
+++ searcher/views.py
@@ -579,17 +579,17 @@ def manage_products(request, brand_id):
 
 ###################################################
 
-def manage_supplier_ingest(request, brand_id):
-    brand = Brand.objects.get(pk=brand_id)
-    SupplierIngestInlineFormSet = inlineformset_factory(Brand, SupplierIngest)
+def manage_supplier_ingest(request, colorstyle=None):
+    images = SupplierIngestImages.objects.get(colorstyle=colorstyle)
+    SupplierIngestInlineFormSet = inlineformset_factory(SupplierIngestImages, SupplierIngest)
     if request.method == "POST" or request.method == "GET":
         formset = SupplierIngestInlineFormSet(request.POST, request.FILES, instance=brand)
         if formset.is_valid():
             formset.save()
             # Do something. Should generally end with a redirect. For example:
-            return HttpResponseRedirect(brand.get_absolute_url())
+            return HttpResponseRedirect(images.get_absolute_url())
     else:
-        formset = SupplierIngestInlineFormSet(instance=brand)
+        formset = SupplierIngestInlineFormSet(instance=images)
     return render_to_response("manage/manage_supplier_ingest.html", {
         "formset": formset,
     })
