8f8434c77b5c1a6c90e4ddff6aaf0737cb3ae65b
johnb <john.bragato@gmail.com>
2014-12-01 10:24:13 -0500

    tasty auth class

--- searcher/api/resources.py
+++ searcher/api/resources.py
@@ -51,6 +51,22 @@ from tastypie.authorization import DjangoAuthorization
 from tastypie.validation import Validation
 from tastypie.resources import ModelResource
 from tastypie.authentication import ApiKeyAuthentication
+
+from requests.auth import AuthBase
+
+class TastypieApiKeyAuth(AuthBase):
+    """
+    Sets the appropriate authentication headers
+    for the Tastypie API key authentication.
+    """
+    def __init__(self, username, api_key):
+        self.username = username
+        self.api_key = api_key
+
+    def __call__(self, r):
+        r.headers['Authorization'] = 'ApiKey %s:%s' % (self.username, self.api_key)
+        return r
+
 ## User and Session Resources
 class UserResource(ModelResource):
     class Meta:
