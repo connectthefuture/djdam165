7fed9c8d139dc83a9509840d233a027cd80cfc0d
johnb <john.bragato@gmail.com>
2014-12-13 16:54:29 -0500

    celery

--- djdam/celery.py
+++ djdam/celery.py
@@ -1,13 +1,13 @@
 from __future__ import absolute_import
  
 import os
-from celery import Celery
+#from celery import Celery
 from django.conf import settings
  
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djdam.settings')
  
-app = Celery('djdam')
+#app = Celery('djdam')
  
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
