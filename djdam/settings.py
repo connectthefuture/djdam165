e09e340fc1cca439d74769a6a4743e798b7edfc2
johnb <jbragato@yahoo.com>
2015-02-09 19:37:50 -0500

    settings

--- djdam/settings.py
+++ djdam/settings.py
@@ -473,8 +473,8 @@ API_LIMIT_PER_PAGE = 20
 # )
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        #'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         #'rest_framework.permissions.IsAuthenticated',
