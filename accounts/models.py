2d47618c4b442874f7a8a89740be3b6492c414de
johnb <jbragato@yahoo.com>
2015-06-24 11:23:29 -0400

    x

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from django.contrib.auth.models import User
 from django.db import models
+from oauth2client.django_orm import FlowField
+
+class FlowModel(models.Model):
+  id = models.ForeignKey(User, primary_key=True)
+  flow = FlowField()
